181a5af95f4f3bf58b7346544fd1ffd2f7ef4cce
Anurag Chemban <anurag.chemban@codezeros.com>
2022-08-03 18:56:32 +0530

    ICO

--- src/App.js
+++ src/App.js
@@ -2,34 +2,68 @@ import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import Popup from 'reactjs-popup';
 import Web3 from 'web3';
-
+import Launch_abi from "./metadata/Launch.json";
 import './App.css';
 
 import CoinDetails from './components/CoinDetails/CoinDetails';
 import ICOlist from './components/IcoList/ICOlist';
 import LaunchICO from './components/LaunchICO/LaunchICO';
-import ERC_abi from "./metadata/ERC.json";
-import ERC_bytecode from "./metadata/ERC_bytecode.json";
 
 
+let web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/v3/93cb9b09ad17492ebf579b891db201c9"));
+
+
+// console.log(List);
 const List = []
 
-let web3 = new Web3.providers.HttpProvider("https://kovan.infura.io/v3/93cb9b09ad17492ebf579b891db201c9");
+// const fetch = async() => {
+//   const contract = new web3.eth.Contract(Launch_abi, "0x86F02E3AD9c061931dEAA7209327Fb3a969bdc00");
+//   const counter = await contract.methods.counter().call();
+ 
+//   for(let i=0; i <= counter; i++){
+//   const parr = await contract.methods.parr(i).call()
+//   List.push(parr);
+//   console.log(parr);
+//   }
+// }
+
 
 function App() {
   const [connected, setConnected] = useState(false)
   const [account, setAccount] = useState("")
   const [connectionStatus, setConnectionStatus] = useState()
+  const [fetchList, setFetchList] = useState(false)
+  const [list, setList] = useState([{}])
+
+//  console.log(list);
+  // fetch()
+
+
+  const fetch = async() => {
+    const contract = new web3.eth.Contract(Launch_abi, "0x86F02E3AD9c061931dEAA7209327Fb3a969bdc00");
+    const counter = await contract.methods.counter().call()
+    console.log(counter);
+
+    for(let i=0; i < counter; i++){
+      const parr = await contract.methods.parr(i).call()
+      List.push(parr);
+      console.log(parr)
+    }
+    setList(list)
+    setFetchList(true)
+  }
+
+ 
 
   // const deploy = async() => {
+  //   console.log(account);
   //   const dep = await new web3.eth.Contract(ERC_abi).deploy({
   //     data: ERC_bytecode,
   //     arguments: [tokenName,tokenSymbol, tokenSupply]
-  //   }).send({from:account[0], gas:3000000});
+  //   }).send({from:account, gas:3000000});
   //   console.log(dep);
   //   alert(dep.address);
   // }
-
 
   const metamask = async() => {
     if (window.ethereum) {
@@ -46,13 +80,11 @@ function App() {
     }
   }
 
-  // useEffect(() => {
-  //   if (connected && account) 
-  //   setConnectionStatus(account);
-  //   else setConnectionStatus("Connect Wallet")
-  //   // data()
-  // })
- 
+  useEffect(() => {
+  metamask()
+  fetch()
+  }, [account])
+
   return (
     <div className="App">
       <header className="appHeader">
@@ -63,10 +95,16 @@ function App() {
         <div className="icoList">
           <h2>Listed ICOs</h2>
           <div className="line"></div>
-          <ICOlist tokenSymbol="SAGA" tokenName="SAGA" tokenAddress="0xe35e0d8be7d7e20fedde7a7e2ed2dc646291855b" tokenSupply="100000" />
-          <ICOlist  tokenSymbol="ABC" tokenName="ABC" tokenAddress="0xe35e0d8be7d7e20fedde7a7e2ed2dc646291855b" tokenSupply="10000000"/>
-          <ICOlist />
-          {/* {List ? (<h4 className="icoNot">No ICO available</h4> ) : <div></div>} */}
+          {/* <ICOlist tokenSymbol="SAGA" tokenName="SAGA" tokenAddress="0xe35e0d8be7d7e20fedde7a7e2ed2dc646291855b" tokenSupply="100000" /> */}
+          {/* <ICOlist  tokenSymbol="ABC" tokenName="ABC" tokenAddress="0xe35e0d8be7d7e20fedde7a7e2ed2dc646291855b" tokenSupply="10000000"/> */}
+      
+          {!fetchList ? (<h4 className="icoNot">No ICO available</h4> ) :( 
+            // List.map((key) => {<ICOlist tokenSymbol={key.symbol} tokenName={key.name} tokenAddress={key.projectAdd} tokenSupply={key.noTokens} tokenStartDate={key.startDate} tokenEndDate={key.endDate} tokenRate={key.rate} />
+            
+            //  console.log(key)})
+          // List.forEach((i) => {<ICOlist tokenSymbol={List[i].symbol} tokenName={List[i].name} tokenAddress={List[i].projectAdd} tokenSupply={List[i].noTokens} tokenStartDate={List[i].startDate} tokenEndDate={List[i].endDate} tokenRate={List[i].rate} />})
+           <ICOlist tokenSymbol={List[0].symbol} tokenName={List[0].name} tokenAddress={List[0].projectAdd} tokenSupply={List[0].noTokens} tokenStartDate={List[0].startDate} tokenEndDate={List[0].endDate} tokenRate={List[0].rate} />
+            )}
         </div>
       </div>
       <div className="footer">
--- src/components/IcoList/ICOlist.js
+++ src/components/IcoList/ICOlist.js
@@ -3,16 +3,17 @@ import Popup from 'reactjs-popup'
 import CoinDetails from '../CoinDetails/CoinDetails'
 import './ICOlist.css'
 
-const ICOlist = ({tokenName, tokenSymbol, tokenAddress, tokenSupply }) => {
+const ICOlist = ({tokenName, tokenSymbol, tokenAddress, tokenSupply, tokenStartDate, tokenEndDate, tokenRate }) => {
+  //  {console.log(tokenSymbol[0])} 
   return (
     <div className="list">
-        <img />
+        <div className="img">{tokenSymbol[0]}</div>
         <h4>{tokenSymbol}</h4>
         <h4>{tokenName}</h4>
         <h4>{tokenAddress}</h4>
         <h4>{tokenSupply}</h4>
-        <Popup trigger={<button>View Details</button>} modal>
-          <div><CoinDetails /></div>
+        <Popup trigger={<button className="detailsButton">View Details</button>} modal>
+          <div><CoinDetails tokenName={tokenName} tokenSymbol={tokenSymbol} tokenAddress={tokenAddress} tokenSupply={tokenSupply} tokenStartDate={tokenStartDate} tokenEndDate={tokenEndDate} tokenRate={tokenRate} /></div>
         </Popup>
     </div>
   )
