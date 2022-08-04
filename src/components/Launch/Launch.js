c858920cd80ecde479d86113bcc87f05bb5d269d
Anurag Chemban <anurag.chemban@codezeros.com>
2022-08-04 18:32:22 +0530

    Launch ICO

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -1,15 +1,110 @@
-import React from 'react'
-import { TextInput } from '../LaunchICO/LaunchICO'
-import './Launch.css'
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import { TextInput } from "../LaunchICO/LaunchICO";
+import IERC from "../../metadata/IERC20.json";
+import ICO from "../../metadata/ICOLaunchpad.json";
+import "./Launch.css";
 
 const Launch = () => {
-  return (
-    <div className="launch">
-        <TextInput label="Supply Amount" value="amount" placeholder="Amount To Supply" />
-        <TextInput label="Token Address" value="address" placeholder="Token Address" />
-        <input type="submit" placeholder="Submit" name="Submit" />   
-    </div>
-  )
-}
+  const [tokenAddress, setTokenAddress] = useState("");
+  const [tokenAmount, setTokenAmount] = useState("");
+  const [rate, setRate] = useState("");
+  const [returnToken, setReturnToken] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [open,setOpen] =useState(true)
 
-export default Launch
+  let listContract = "0x5C8EfC806b9AA0F3C0F0F135F1c2772690519357";
+
+  const list = async () => {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    //Approval
+    let TContract = new ethers.Contract(tokenAddress, IERC.abi, provider);
+    let Tsigner = TContract.connect(signer);
+    await Tsigner.approve(listContract, tokenAmount.toString());
+
+    //List
+    let LContract = new ethers.Contract(listContract, ICO.abi, provider);
+    let LSigner = LContract.connect(signer);
+    await LSigner.listProject(
+      tokenAddress.toString(),
+      tokenAmount.toString(),
+      rate.toString(),
+      returnToken.toString(),
+      startDate.toString(),
+      endDate.toString()
+    );
+  };
+
+  const closeModal = () => {
+    setOpen(false)
+  }
+  
+  return (
+   <div>
+      {open && (
+    <div className="launch">
+       <button className="close" onClick={closeModal}>          
+        &times;        
+      </button>
+    <TextInput
+      onChange={(e) => {
+        setTokenAmount(e.target.value);
+      }}
+      label="Supply Amount"
+      value={tokenAmount}
+      placeholder="Amount To Supply"
+    />
+    <TextInput
+      onChange={(e) => {
+        setTokenAddress(e.target.value);
+      }}
+      label="Token Address"
+      value={tokenAddress}
+      placeholder="Token Address"
+    />
+    <TextInput
+      onChange={(e) => {
+        setRate(e.target.value);
+      }}
+      label="Rate"
+      value={rate}
+      placeholder="Rate"
+    />
+    <TextInput
+      onChange={(e) => {
+        setReturnToken(e.target.value);
+      }}
+      label="Tokens"
+      value={returnToken}
+      placeholder="Number of Tokens"
+    />
+    <TextInput
+      onChange={(e) => {
+        setStartDate(e.target.value);
+      }}
+      label="Start Date"
+      value={startDate}
+      placeholder="Start date in Epoch"
+    />
+    <TextInput
+      onChange={(e) => {
+        setEndDate(e.target.value);
+      }}
+      label="End Date"
+      value={endDate}
+      placeholder="End Date in Epoch"
+    />
+    <button onClick={list}>Submit</button>
+  </div>)}
+   </div>
+     
+  );
+};
+
+export default Launch;
--- src/components/LaunchICO/LaunchICO.js
+++ src/components/LaunchICO/LaunchICO.js
@@ -1,28 +1,134 @@
-import React from 'react'
-import Popup from 'reactjs-popup'
-import Launch from '../Launch/Launch'
-import './LaunchICO.css'
+import React, { useState } from "react";
+import Popup from "reactjs-popup";
+import Launch from "../Launch/Launch";
+import { ethers, Wallet } from "ethers";
+import EthereumTx from "@ethereumjs/tx";
+import ERC from "../../metadata/ERC.json";
+import "./LaunchICO.css";
 
-export const TextInput = ({label, placeholder}) => {
-    return(
-        <div className='textContainer'>
-            <label>{label}</label>
-            <input type="text" placeholder={placeholder} /> 
-        </div>
-    )
-}
+export const TextInput = ({ label, placeholder, onChange }) => {
+  return (
+    <div className="textContainer">
+      <label>{label} : </label>
+      <input type="text" placeholder={placeholder} onChange={onChange} />
+    </div>
+  );
+};
+
+// const rpcURL = "http://127.0.0.1:7545";
+// // const web3 = new Web3(rpcURL);
+// let web3 = new Web3(
+//   new Web3.providers.HttpProvider(
+//     "https://kovan.infura.io/v3/93cb9b09ad17492ebf579b891db201c9"
+//   )
+// );
 
 const LaunchICO = () => {
-  return (
-    <div className='modalContainer'>
-      <Popup modal trigger={<button>Already have a token? </button>}><Launch /></Popup>
-      <div className="line"></div>
-      <TextInput name="tokenName" label="Token Name" placeholder="Token Name" />
-      <TextInput name="tokenSymbol" label="Token Symbol" placeholder="Token Symbol" />
-      <TextInput name="initialAmount" label="Initial Amount" placeholder="Initial Amount" />
-      <button>Mint</button>
-    </div>
-  )
-}
+  const [connected, setConnected] = useState(false);
+  const [account, setAccount] = useState("");
+  const [tokenName, setTokenName] = useState("");
+  const [tokenSymbol, setTokenSymbol] = useState("");
+  const [initalAmount, setInitalAmount] = useState("");
+  const [contractAddress, setContractAddress] =useState("")
+  const [open, setOpen] = useState(true);
 
-export default LaunchICO
+  // let provider = ethers.getDefaultProvider('kovan');
+  // let privateKey =  "d1277ddf595a6b84849582d89df944f3503ad5c2c8b06ec14ca6fad5401b7317"
+  // let wallet = new ethers.Wallet(privateKey, provider);
+
+  const deploy = async () => {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    let factory = new ethers.ContractFactory(ERC.abi, ERC.bytecode, signer);
+    let contract = await factory.deploy(
+      initalAmount.toString(),
+      tokenName.toString(),
+      tokenSymbol.toString()
+    );  
+    setOpen(false)
+    await contract.deployed();
+    alert(contract.address);
+  };
+
+  // const metamask = async () => {
+  //   if (window.ethereum) {
+  //     if (!connected) {
+  //       const accounts = await window.ethereum.request({
+  //         method: "eth_requestAccounts",
+  //       });
+  //       setAccount(accounts[0]);
+  //       setConnected(true);
+  //       // console.log(account);
+  //     } else {
+  //     }
+  //   } else {
+  //     alert("Install Metamask extension!!!");
+  //   }
+  // };
+  // metamask();
+
+  // const deploy = async () => {
+  //   console.log(account);
+  //   console.log(initalAmount, tokenName, tokenSymbol);
+  //   // const dep = await new web3.eth.Contract(ERC.abi)
+  //     window.ethereum.request({ method: "eth_sendTransaction" })
+  //     .deploy({
+  //       data: ERC.bytecode,
+  //       arguments: [initalAmount, tokenName, tokenSymbol],
+  //     })
+  //     .send({ from: account, gas: 3000000 })
+  //     // onsole.log(dep);
+  //   // alert(dep.address);
+  // };
+  const closeModal = () => {
+    setOpen(false);
+  }
+
+  return (
+    <div>
+      {
+      open && (<div className="modalContainer">
+      <button className="close" onClick={closeModal}>          
+        &times;        
+      </button>
+      <h1>Launch ERC20 Token</h1>
+      <div className="line"></div>
+      <TextInput
+        value={tokenName}
+        onChange={(e) => {
+          setTokenName(e.target.value);
+        }}
+        name="tokenName"
+        label="Token Name"
+        placeholder="Token Name"
+      />
+      <TextInput
+        value={tokenSymbol}
+        onChange={(e) => {
+          setTokenSymbol(e.target.value);
+        }}
+        name="tokenSymbol"
+        label="Token Symbol"
+        placeholder="Token Symbol"
+      />
+      <TextInput
+        value={initalAmount}
+        onChange={(e) => {
+          setInitalAmount(e.target.value);
+        }}
+        name="initialAmount"
+        label="Initial Amount"
+        placeholder="Initial Amount"
+      />
+      <button onClick={() => {deploy(); closeModal();}}>Mint</button>
+    </div>)
+    }
+    
+    </div>
+  );
+};
+
+export default LaunchICO;
